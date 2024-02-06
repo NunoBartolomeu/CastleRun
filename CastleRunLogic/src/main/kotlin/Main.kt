4a3448eb1640d6aff1856e329b126e61af104538
Reyyorker <reyyorcker@gmail.com>
2024-02-06 15:14:40 +0000

    updatred board
    board visualazable, board has entries and exit

--- CastleRunLogic/src/main/kotlin/Main.kt
+++ CastleRunLogic/src/main/kotlin/Main.kt
@@ -1,16 +1,20 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import org.example.models.Board
+import org.example.models.Tile
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main() {
+    val numRows = 5
+    val numCols = 9
+    val tiles: Array<Array<Tile>> = Array(numRows) { Array(numCols) { Tile.Floor } }
+    tiles [0][1] = Tile.Entry
+    tiles [4][6] = Tile.Exit
+    tiles [4][3] = Tile.Wall
+    tiles [3][4] = Tile.Wall
+    tiles [0][0] = Tile.Entry
+    tiles [3][0] = Tile.Wall
+    var board = Board(tiles,null)
+
+    board.visualization()
+
 }
--- CastleRunLogic/src/main/kotlin/models/Board.kt
+++ CastleRunLogic/src/main/kotlin/models/Board.kt
@@ -1,8 +1,47 @@
 package org.example.models
 
-data class Board(val tiles: Array<Array<Tile>>, val equipment: Map<Coords, Equipment> ) {
+data class Board(val tiles: Array<Array<Tile>>, val equipment: Map<Coords, Equipment>? ) {
     //TODO make a simple 9 por 18 board
     //TODO set the entrances list and the exits list
+    val entries: MutableList<Coords> = mutableListOf()
+    val exits : MutableList<Coords> = mutableListOf()
+
+    init {
+        //run through the tiles and save all entries to the list
+
+        //Por cada row fazer,
+        tiles.forEachIndexed { rowIdx, row ->
+            //Por cada col fazer,
+
+            row.forEachIndexed { colIdx, col ->
+                //Se o tile == Entry
+
+                if (tiles[rowIdx][colIdx] == Tile.Entry)
+                //Guardar tile entry
+                    entries.add(Coords(rowIdx, colIdx))
+
+                if (tiles[rowIdx][colIdx] == Tile.Exit)
+                //Guardar tile exit
+                    exits.add(Coords(rowIdx, colIdx))
+            }
+        }
+    }
+    fun visualization() {
+        for (row in this.tiles) {
+            for (tile in row) {
+                print(when (tile) {
+                    Tile.Wall ->"1"
+                    Tile.Floor ->"2"
+                    Tile.Entry ->"3"
+                    Tile.Exit ->"4"
+                })
+            }
+            println()
+        }
+    }
+
+
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -21,3 +60,4 @@ data class Board(val tiles: Array<Array<Tile>>, val equipment: Map<Coords, Equip
         return result
     }
 }
+
