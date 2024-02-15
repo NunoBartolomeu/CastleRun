9f77d1a811bcb8eb9efa248702671cfe1364ab88
NunoBartolomeu <a47233@alunos.isel.pt>
2024-02-15 03:09:39 +0000

    Add Utility functions and board models

---
+++ CastleRunLogic/src/main/kotlin/models/board/MutableTile.kt
@@ -0,0 +1,4 @@
+package org.example.models.board
+
+class MutableTile {
+}
--- CastleRunLogic/src/main/kotlin/models/board/Neighbour.kt
+++
@@ -1,6 +0,0 @@
-package models.board
-
-import org.example.models.board.Direction
-import org.example.models.board.Tile
-
-data class Neighbour(val tile: Tile, val direction: Direction)
